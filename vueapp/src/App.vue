2492c90e690e4ad972382e4227b479d1fcbe999f
Roberto Torres <roberto.torres.dev@gmail.com>
2017-10-14 08:11:15 -0700

    my first component :D

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello-world/>
+    <test/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Test from "./components/Test"
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Test
   }
 }
 </script>
